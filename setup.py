bee647b79d074d6dfe9f1a116c325ea3495f7cd7
Andrey Fedorov <andrey.fedorov@snowflake.com>
2021-04-08 18:49:36 -0700

    Initial sqlpack runner + Okta ingest

---
+++ setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+
+setup(
+    name='sqlpack',
+    packages=['sqlpack'],
+    version='0.1',
+    license='apache-2.0',
+    description='The sqlpack module lets you distribute modular SQL code.',
+    author='Siddhil Rathod',
+    author_email='siddhi.rathod@snowflake.com',
+    url='https://github.com/Snowflake-Labs/sqlpack',
+    scripts=['bin/sqlpack'],
+    keywords='SNOWFLAKE PACKS SNOWSQL SNOWSQL-PACKS',
+    install_requires=[
+        'fire',
+        'pyyaml',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: apache-2.0 License',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
