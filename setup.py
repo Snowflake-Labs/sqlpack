e7aac0a757ae119056769ab14404f2eb2135f222
sfc-gh-afedorov <42752788+sfc-gh-afedorov@users.noreply.github.com>
2021-04-22 14:45:13 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,11 +10,9 @@ setup(
     author='Siddhil Rathod',
     author_email='siddhi.rathod@snowflake.com',
     url='https://github.com/Snowflake-Labs/sqlpack',
-    download_url = 'https://github.com/Snowflake-Labs/sqlpacks/archive/refs/tags/0.1.0.tar.gz',
+    download_url='https://github.com/Snowflake-Labs/sqlpacks/archive/refs/tags/0.1.0.tar.gz',
     scripts=['bin/sqlpack'],
-    package_data={
-        "": ["*.py","packs/*/*.sql.fmt","packs/*/*.yaml"]
-    },
+    package_data={"sqlpack": ["*.py", "packs/*/*.sql.fmt", "packs/*/*.yaml"]},
     include_package_data=True,
     keywords='SNOWFLAKE PACKS SNOWSQL SNOWSQL-PACKS',
     install_requires=[
