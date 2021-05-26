159804c17ded3a41fef4eb53e59308c579be41cb
sfc-gh-srathod <siddhil.rathod@snowflake.com>
2021-05-26 18:31:05 +0530

    added condition to the list function

--- sqlpack/pack.py
+++ sqlpack/pack.py
@@ -18,7 +18,12 @@ SQLPACK_PATH = [
 def list():
     packs_dir = path.join(path.dirname(__file__), '..', 'packs')
     if path.exists(packs_dir):
-        packs = listdir(packs_dir)
+        directories = listdir(packs_dir)
+        packs = []
+        for pack in directories:
+            content = listdir(path.join(packs_dir,pack))
+            if "main.sql.fmt" in content:
+                packs.append(pack)
         packs.sort()
         return packs
     return "Packs directory not found"
