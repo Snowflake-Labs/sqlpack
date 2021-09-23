ed68c0fe2dec208880f3d65e575233bb703bd6a6
Prasanth Kommini <prasanth.kommini@snowflake.com>
2021-09-23 10:36:46 -0700

    Add ef-smtp

--- packs/snowalert/results-alert-dispatcher.js
+++ packs/snowalert/results-alert-dispatcher.js
@@ -1,31 +1,30 @@
-
 // args
 
 // library
 
-function exec(sqlText, binds=[]) {
-  let retval = []
-  const stmnt = snowflake.createStatement({sqlText, binds})
-  const result = stmnt.execute()
+function exec(sqlText, binds = []) {
+  let retval = [];
+  const stmnt = snowflake.createStatement({ sqlText, binds });
+  const result = stmnt.execute();
   const columnCount = stmnt.getColumnCount();
-  const columnNames = []
-  for (let i = 1 ; i < columnCount + 1 ; i++) {
-    columnNames.push(stmnt.getColumnName(i))
+  const columnNames = [];
+  for (let i = 1; i < columnCount + 1; i++) {
+    columnNames.push(stmnt.getColumnName(i));
   }
 
-  while(result.next()) {
+  while (result.next()) {
     let o = {};
     for (let c of columnNames) {
-      o[c] = result.getColumnValue(c)
+      o[c] = result.getColumnValue(c);
     }
-    retval.push(o)
+    retval.push(o);
   }
-  return retval
+  return retval;
 }
 
 // business logic
 
-RUN_TABLE = 'results.handled_alerts'
+RUN_TABLE = "results.handled_alerts";
 
 HANDLE_ALERTS = `
 CREATE TEMP TABLE results.handled_alerts AS
@@ -39,6 +38,8 @@ SELECT
       THEN jira_handler(alert, value)
       WHEN value['type'] = 'ef-jira-comment'
       THEN jira_comment_handler(alert, value)
+      WHEN value['type'] = 'ef-smtp'
+      THEN smtp_handler(value)
       ELSE OBJECT_CONSTRUCT(
         'error', 'missing handler',
         'handler', value['type']
@@ -66,26 +67,26 @@ FROM (
 WHERE value['type'] IN (
   'ef-slack',
   'ef-jira',
-  'ef-jira-comment'
+  'ef-jira-comment',
+  'ef-smtp'
 )
 GROUP BY id
-`
+`;
 
-COUNT_HANDLED = `SELECT COUNT(*) n FROM ${RUN_TABLE}`
+COUNT_HANDLED = `SELECT COUNT(*) n FROM ${RUN_TABLE}`;
 
 HANDLE_ALL = `
 MERGE INTO results.alerts d
 USING ${RUN_TABLE} s
 ON (d.alert_id = s.alert_id)
 WHEN MATCHED THEN UPDATE SET d.handled=s.handled
-`
+`;
 
 return {
-  'handle_alerts': exec(HANDLE_ALERTS),
-  'handled': (
-    exec(COUNT_HANDLED)[0]['N'] > 0
+  handle_alerts: exec(HANDLE_ALERTS),
+  handled:
+    exec(COUNT_HANDLED)[0]["N"] > 0
       ? exec(HANDLE_ALL)
-      : {'number of rows updated': 0, 'number of rows inserted': 0}
-  ),
-  'dropped': exec('DROP TABLE results.handled_alerts'),
-}
+      : { "number of rows updated": 0, "number of rows inserted": 0 },
+  dropped: exec("DROP TABLE results.handled_alerts"),
+};
