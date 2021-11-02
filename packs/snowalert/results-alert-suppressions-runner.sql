1cef0e7297e5dd340e6680e7c34b1754026be5da
Andrey <42752788+sfc-gh-afedorov@users.noreply.github.com>
2021-11-02 11:45:19 -0700

    Update results-alert-suppressions-runner.sql

--- packs/snowalert/results-alert-suppressions-runner.sql
+++ packs/snowalert/results-alert-suppressions-runner.sql
@@ -1,13 +1,20 @@
-CREATE OR REPLACE PROCEDURE results.alert_suppressions_runner(queries_like STRING)
-RETURNS VARIANT
-LANGUAGE JAVASCRIPT
-EXECUTE AS CALLER
-USING TEMPLATE 'results-alert-suppressions-runner.js'
-;
+-- CREATE OR REPLACE PROCEDURE results.alert_suppressions_runner(queries_like STRING)
+-- RETURNS VARIANT
+-- LANGUAGE JAVASCRIPT
+-- EXECUTE AS CALLER
+-- USING TEMPLATE 'results-alert-suppressions-runner.js'
+-- ;
 
 CREATE OR REPLACE PROCEDURE results.alert_suppressions_runner()
 RETURNS VARIANT
 LANGUAGE JAVASCRIPT
 EXECUTE AS CALLER
 USING TEMPLATE 'results-alert-suppressions-runner.js'
+;
+
+CREATE OR REPLACE TASK results.suppressions_merge 
+  WAREHOUSE={snowalert_warehouse}
+  AFTER results.alerts_merge
+AS
+CALL results.alert_suppressions_runner()
 ;
