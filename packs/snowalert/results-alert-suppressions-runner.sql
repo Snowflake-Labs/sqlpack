30974b1b8da8503957e87c54d64d7e403db4472f
Andrey <42752788+sfc-gh-afedorov@users.noreply.github.com>
2021-11-02 11:47:38 -0700

    Update results-alert-suppressions-runner.sql

--- packs/snowalert/results-alert-suppressions-runner.sql
+++ packs/snowalert/results-alert-suppressions-runner.sql
@@ -1,3 +1,10 @@
+CREATE OR REPLACE PROCEDURE results.alert_suppressions_runner(queries_like STRING)
+RETURNS VARIANT
+LANGUAGE JAVASCRIPT
+EXECUTE AS CALLER
+USING TEMPLATE 'results-alert-suppressions-runner.js'
+;
+
 CREATE OR REPLACE PROCEDURE results.alert_suppressions_runner()
 RETURNS VARIANT
 LANGUAGE JAVASCRIPT
