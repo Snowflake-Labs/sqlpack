5395f1d6f05996e8d42e7f84d5113ae8faddc3f0
sfc-gh-nlele <nachiket.lele@snowflake.com>
2022-04-12 22:05:38 +0530

    Interpolate alert id in the update statement

--- packs/snowalert/results-alert-dispatcher.js
+++ packs/snowalert/results-alert-dispatcher.js
@@ -90,9 +90,9 @@ return exec(GET_HANDLERS).rows.map((h) => {
        ${handler_num},
        ${handler_name}(PARSE_JSON(?), PARSE_JSON(?))
      )
-     WHERE alert_id=?
+     WHERE alert_id=${alert_id}
     `,
-    [alert, payload, alert_id]
+    [alert, payload]
   )
   
   return {
