17c0b473162c24f9db2f08285182ee81fa830b09
Andrey <42752788+sfc-gh-afedorov@users.noreply.github.com>
2023-03-20 20:13:38 -0700

    Update results-alert-scheduler.sql

--- packs/snowalert/results-alert-scheduler.sql
+++ packs/snowalert/results-alert-scheduler.sql
@@ -4,3 +4,9 @@ LANGUAGE JAVASCRIPT
 EXECUTE AS CALLER
 USING TEMPLATE 'results-alert-scheduler.js'
 ;
+
+CREATE OR REPLACE TASK results.schedule_alerts
+WAREHOUSE=snowalert_warehouse
+SCHEDULE='USING CRON 1/15 * * * * UTC'
+AS CALL results.alert_scheduler('snowalert_warehouse')
+;
